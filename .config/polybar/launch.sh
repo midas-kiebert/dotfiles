2d17417a42052793cd33209145ed9fd83b9c55ee
Midas Kiebert <midascubert@gmail.com>
2022-05-17 00:34:08 +0200

    fixed 2nd display polybar module launhc

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,13 +2,14 @@
 
 killall -q polybar
 
-$second=$(xrandr --listmonitors | grep " DP"| sed -n -e 's/.*  //p') 
-
+second=$(xrandr --listmonitors | grep " DP"| sed -n -e 's/.*  //p') 
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 if [ -z "$second" ]
 then
+	echo "single monitor"
 	MONITOR=$(xrandr --listmonitors | grep " eDP"| sed -n -e 's/.*  //p') polybar main &
 else
+	echo "dual monitor"
 	MONITOR=$(xrandr --listmonitors | grep " DP"| sed -n -e 's/.*  //p') polybar DisplayPort2 &
 	MONITOR=$(xrandr --listmonitors | grep " eDP"| sed -n -e 's/.*  //p') polybar main &
 fi
